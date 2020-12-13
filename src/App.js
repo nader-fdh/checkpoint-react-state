945f1dcd81417a95ef2bf0cd5bd390985215435a
nader-fdh <naderfadhelofficiel@gmail.com>
2020-12-13 12:06:05 -1100

    adding react-state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import react, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state= {
+    statut:false ,
+    fullName : 'Fadhel Nader',
+    bio : 'Devlopper',
+    profession : 'FullStack js Dev',
+    img : 'https://scontent.ftun3-1.fna.fbcdn.net/v/t1.0-9/118063791_1497914803748532_1300174053195448940_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=enR3DbxtpjIAX8GRz8P&_nc_ht=scontent.ftun3-1.fna&oh=9b321fc1bbe892b352e08a234bac14d8&oe=5FFCC450',
+
+  } ;
+  render() {
+return(
+   <div className='App'>
+     <button onClick={() => this.setState({statut:!this.state.statut})}>Click me</button>
+
+     {this.state.statut 
+      ? 
+      <div className="card">
+        <div className="image">
+            <img src={this.state.img} alt="image"/>
+        </div>
+        <div className="contents">
+      <h2>{this.state.fullName}<br/><p>{this.state.profession+' junior '+this.state.bio}</p></h2>
+            
+        </div>
+      </div>
+      :<h1 style={{color:'white'}}> Click the Button</h1>}
+   </div>
+     
+
+)}
 }
 
 export default App;
